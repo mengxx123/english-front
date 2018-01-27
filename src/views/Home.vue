c8080b1047893536190b75ee8e06ecbd76ceb014
cjh <1418503647@qq.com>
2018-01-27 21:58:08 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+    <my-page title="首页">
+        <div class="card" @click="playAudio">
+            {{ letter }}<span class="lowercase">{{ lowerCase }}</span>
+        </div>
+        <ul class="letter-list">
+            <li class="item"
+                @click="setLetter(letter)"
+                v-for="letter in letters">{{ letter }}</li>
+        </ul>
+        <audio :src="audioSrc" ref="audio"></audio>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                letter: 'A',
+                letters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+                audioSrc: ''
+            }
+        },
+        computed: {
+            lowerCase() {
+                return this.letter.toLowerCase()
+            }
+        },
+        methods: {
+            setLetter(letter) {
+                this.letter = letter
+            },
+            playAudio() {
+                this.audioSrc = `http://img1.yunser.com/letter/${this.letter.toLowerCase()}.mp3`
+                this.$nextTick(() => {
+                    this.$refs.audio.play()
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../scss/var";
+
+    .card {
+        position: relative;
+        width: 180px;
+        height: 240px;
+        margin-bottom: 16px;
+        padding: 16px;
+        margin-right: 8px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        background-color: #fff;
+        cursor: pointer;
+        //
+        font-size: 100px;
+        text-align: center;
+        line-height: 200px;
+        color: #00b6f7;
+        .lowercase {
+            font-size: 80px;
+        }
+    }
+    .letter-list {
+        @include clearfix;
+        .item {
+            float: left;
+            width: 40px;
+            height: 40px;
+            margin-right: 8px;
+            margin-bottom: 8px;
+            text-align: center;
+            line-height: 40px;
+            background-color: #fff;
+            border: 1px solid #eee;
+            cursor: pointer;
+        }
+    }
+</style>
